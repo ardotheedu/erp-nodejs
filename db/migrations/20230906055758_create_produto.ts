3bb88a275b55d71b84f9de16ca1c5c17954a6db5
devgabrielb1 <dev.gabriel@gmail.com>
2023-09-10 23:17:01 -0300

    feat: add migrations of stock

--- db/migrations/20230906055758_create_produto.ts
+++ db/migrations/20230906055758_create_produto.ts
@@ -2,9 +2,26 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('produto', (table)=>{
+        table.uuid('id').primary()
+        table.string('nome').notNullable()
+        table.string('descricao').nullable()
+        table.decimal("preco_unitario", 10, 2).notNullable()
+        table.decimal("preco_venda", 10, 2).notNullable()
+        table.integer("quantidade").notNullable()
+        table.timestamp("data_validade").notNullable()
+        table.string('lote').notNullable()
+
+        table.uuid("produto_categoria_id").notNullable();
+        table
+        .foreign("produto_categoria_id")
+        .references("id")
+        .inTable("categoria");
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("produto");
 }
 
--- db/migrations/20230906055817_create_categoria.ts
+++ db/migrations/20230906055817_create_categoria.ts
@@ -2,9 +2,14 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('categoria', (table)=>{
+        table.uuid('id').primary()
+        table.string('nome').notNullable()
+    })
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("categoria");
 }
 
