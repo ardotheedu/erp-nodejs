5f490cbb8d5e250dbbf3ee15c19b82ccd2e252a2
devgabrielb1 <dev.gabriel@gmail.com>
2023-09-13 01:03:55 -0300

    refactor: add more routes

--- src/Modules/Stock/Routes/index.ts
+++ src/Modules/Stock/Routes/index.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from 'fastify'
-import { categoryRoutes } from './category'
+import { categoryRoutes } from './categoryRoute'
+import { supplierRoutes } from './supplierRoute'
+import { productRoutes } from './productRoute'
 
 export async function registerStockRoutes(app: FastifyInstance) {
   
-  app.register(categoryRoutes,{prefix:'category'})
+  app.register(categoryRoutes,{prefix:'categories'})
+  app.register(supplierRoutes,{prefix:'suppliers'})
+  app.register(productRoutes,{prefix:'products'})
 
 }
 
