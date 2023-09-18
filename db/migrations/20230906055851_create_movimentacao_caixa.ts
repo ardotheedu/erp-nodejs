9700dae16cca7af13c58416284aaa932513d7fd1
Bernard2307 <bernardlnogueira@outlook.com>
2023-09-18 20:18:21 -0300

    fix: corrigi a tabela movimentacao_caixa
    fix: troquei o preenchimento do SEED de movi_caixa

--- db/migrations/20230906055851_create_movimentacao_caixa.ts
+++ db/migrations/20230906055851_create_movimentacao_caixa.ts
@@ -3,19 +3,9 @@ import { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('movimentacao_caixa', (table) => {
     table.uuid('id').primary()
-    table.string('nome').notNullable()
-    table.string('descricao').nullable()
-    table
-      .enum('tipo_movimentacao_caixa', ['entrada', 'saida', 'devolucao'])
-      .notNullable()
-    table.integer('quantidade').notNullable()
-    table.timestamp('data_movimentacao_caixa').notNullable()
-
-    table
-      .uuid('movimentacao_caixa_produto_id')
-      .notNullable()
-      .references('id')
-      .inTable('produto')
+    table.timestamp('timestamp').notNullable()
+    table.enum('tipo', ['entrada', 'saida']).notNullable()
+    table.decimal('valor').notNullable()
   })
 }
 
