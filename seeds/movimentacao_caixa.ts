f013293f4e362def1a7d840d4a69feaac057e3ff
Bernard2307 <bernardlnogueira@outlook.com>
2023-09-24 09:40:28 -0300

    Preenchimento das tabelas

--- seeds/movimentacao_caixa.ts
+++ seeds/movimentacao_caixa.ts
@@ -25,5 +25,47 @@ export async function seed(knex: Knex): Promise<void> {
       tipo: 'devolucao',
       valor: 25.00,
     },
+    {
+      id: randomUUID(),
+      timestamp: new Date(),
+      tipo: 'entrada',
+      valor: 75.00,
+    },
+    {
+      id: randomUUID(),
+      timestamp: new Date(),
+      tipo: 'saida',
+      valor: 30.00,
+    },
+    {
+      id: randomUUID(),
+      timestamp: new Date(),
+      tipo: 'devolucao',
+      valor: 15.00,
+    },
+    {
+      id: randomUUID(),
+      timestamp: new Date(),
+      tipo: 'entrada',
+      valor: 120.00,
+    },
+    {
+      id: randomUUID(),
+      timestamp: new Date(),
+      tipo: 'saida',
+      valor: 60.00,
+    },
+    {
+      id: randomUUID(),
+      timestamp: new Date(),
+      tipo: 'entrada',
+      valor: 90.00,
+    },
+    {
+      id: randomUUID(),
+      timestamp: new Date(),
+      tipo: 'saida',
+      valor: 45.00,
+    },
   ])
 }
--- seeds/notafiscal.ts
+++ seeds/notafiscal.ts
@@ -13,5 +13,38 @@ export async function seed(knex: Knex): Promise<void> {
       id: randomUUID(),
       numero_nota: "10987654321",
     },
+    {
+      id: randomUUID(),
+      numero_nota: "5678901234",
+    },
+    {
+      id: randomUUID(),
+      numero_nota: "4321098765",
+    },
+    {
+      id: randomUUID(),
+      numero_nota: "9876543210",
+    },
+    {
+      id: randomUUID(),
+      numero_nota: "2468135790",
+    },
+    {
+      id: randomUUID(),
+      numero_nota: "1357924680",
+    },
+    {
+      id: randomUUID(),
+      numero_nota: "8642097531",
+    },
+    {
+      id: randomUUID(),
+      numero_nota: "1122334455",
+    },
+    {
+      id: randomUUID(),
+      numero_nota: "9988776655",
+    },
   ]);
 }
+
