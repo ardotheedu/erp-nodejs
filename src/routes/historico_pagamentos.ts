917f927c65fc7cc9c0e87ffd6035a6c9585ceb29
Bernard2307 <bernardlnogueira@outlook.com>
2023-10-25 17:17:37 -0300

    create: routes

--- src/routes/historico_pagamentos.ts
+++ src/routes/historico_pagamentos.ts
@@ -1,109 +1,26 @@
-import { FastifyInstance } from "fastify";
-import { z } from "zod";
-import { randomUUID } from "node:crypto";
-import { knex } from "../database";
-import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
-export async function historicoPagamentosRoutes(app: FastifyInstance) {
-  app.get(
-    '/cliente/:id/historico',
-    {
-      preHandler: [checkSessionIdExists],
-    },
-    async (request, reply) => {
-      const getHistoricoPagamentosParamsSchema = z.object({
-        id: z.string().uuid(),
-      });
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { knex } from '../database';
 
-      try {
-        const { id } = getHistoricoPagamentosParamsSchema.parse(request.params);
-        const historicoPagamentos = await knex('historico_pagamentos')
-          .where({ cliente_id: id }) 
-          .select('*');
-
-        return historicoPagamentos;
-      } catch (error) {
-        console.error(error);
-        return reply.status(500).send('Erro ao obter o histórico de pagamentos do cliente.');
-      }
+export async function historicoVendasRoutes(app: FastifyInstance) {
+  app.get('/', async (request, reply) => {
+    try {
+      const historicoVendas = await knex('historico_vendas').select('*');
+      return { historicoVendas };
+    } catch (error) {
+      console.error(error);
+      reply.code(500).send('Erro ao buscar histórico de vendas.');
     }
-  );
-  app.get(
-    '/pagamentos-atraso',
-    {
-      preHandler: [checkSessionIdExists],
-    },
-    async (request, reply) => {
-      try {
-        const historicoPagamentosAtraso = await knex('historico_pagamentos')
-          .where('data', '<', new Date())  
-          .select('*');
+  });
 
-        return historicoPagamentosAtraso;
-      } catch (error) {
-        console.error(error);
-        return reply.status(500).send('Erro ao obter o histórico de pagamentos em atraso.');
-      }
+  app.post('/', async (request, reply) => {
+    try {
+      const novoHistoricoVenda = request.body;
+      const [id] = await knex('historico_vendas').insert(novoHistoricoVenda);
+      reply.code(201).send({ id });
+    } catch (error) {
+      console.error(error);
+      reply.code(500).send('Erro ao criar histórico de venda.');
     }
-  );
-
-  app.get(
-    '/pagamentos-pagos',
-    {
-      preHandler: [checkSessionIdExists],
-    },
-    async (request, reply) => {
-      try {
-        const historicoPagamentosPagos = await knex('historico_pagamentos')
-          .where('data', '>=', new Date())  
-          .select('*');
-
-        return historicoPagamentosPagos;
-      } catch (error) {
-        console.error(error);
-        return reply.status(500).send('Erro ao obter o histórico de pagamentos pagos.');
-      }
-    }
-  );
-  app.get(
-    '/pagamentos-a-vencer',
-    {
-      preHandler: [checkSessionIdExists],
-    },
-    async (request, reply) => {
-      try {
-        const historicoPagamentosAVencer = await knex('historico_pagamentos')
-          .where('data', '>', new Date())
-          .select('*');
-
-        return historicoPagamentosAVencer;
-      } catch (error) {
-        console.error(error);
-        return reply.status(500).send('Erro ao obter o histórico de pagamentos a vencer.');
-      }
-    }
-  );
-  app.get(
-    '/pagamentos-por-data',
-    {
-      preHandler: [checkSessionIdExists],
-    },
-    async (request, reply) => {
-      const getHistoricoPagamentosPorDataQuerySchema = z.object({
-        start_date: z.string(),
-        end_date: z.string(),
-      });
-
-      try {
-        const { start_date, end_date } = getHistoricoPagamentosPorDataQuerySchema.parse(request.query);
-        const historicoPagamentosPorData = await knex('historico_pagamentos')
-          .whereBetween('data', [start_date, end_date])
-          .select('*');
-
-        return historicoPagamentosPorData;
-      } catch (error) {
-        console.error(error);
-        return reply.status(500).send('Erro ao obter o histórico de pagamentos por intervalo de datas.');
-      }
-    }
-  );
+  });
 }
--- src/routes/historico_vendas.ts
+++ src/routes/historico_vendas.ts
@@ -1,30 +1,28 @@
-import { FastifyInstance } from "fastify";
-import { z } from "zod";
-import { randomUUID } from "node:crypto";
-import { knex } from "../database";
-import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
-export async function historicoVendasRoutes(app: FastifyInstance) {
-  app.get(
-    '/venda-saida/:id/historico',
-    {
-      preHandler: [checkSessionIdExists],
-    },
-    async (request, reply) => {
-      const getHistoricoVendasParamsSchema = z.object({
-        id: z.string().uuid(),
-      });
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { knex } from '../database';
 
-      try {
-        const { id } = getHistoricoVendasParamsSchema.parse(request.params);
-        const historicoVendas = await knex('historico_vendas')
-          .where({ venda_saida_id: id })  
-          .select('*');
-
-        return historicoVendas;
-      } catch (error) {
-        console.error(error);
-        return reply.status(500).send('Erro ao obter o histórico de vendas da venda de saída.');
-      }
+export async function historicoPagamentosRoutes(app: FastifyInstance) {
+  // Rota para obter todos os históricos de pagamentos (GET)
+  app.get('/', async (request, reply) => {
+    try {
+      const historicoPagamentos = await knex('historico_pagamentos').select('*');
+      reply.send({ historicoPagamentos });
+    } catch (error) {
+      console.error(error);
+      reply.code(500).send('Erro ao buscar histórico de pagamentos.');
     }
-  );
+  });
+
+  // Rota para criar um novo histórico de pagamento (POST)
+  app.post('/', async (request, reply) => {
+    try {
+      const novoHistoricoPagamento = request.body;
+      const [id] = await knex('historico_pagamentos').insert(novoHistoricoPagamento);
+      reply.code(201).send({ id });
+    } catch (error) {
+      console.error(error);
+      reply.code(500).send('Erro ao criar histórico de pagamento.');
+    }
+  });
 }
