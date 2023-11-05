85ec5f0f31e754afc7b77848a6c4f55c302419ba
ardotheedu <eduardosalima3@gmail.com>
2023-11-04 21:52:46 -0300

    feat: adiciona rotas

--- src/Modules/RH/routes/index.ts
+++ src/Modules/RH/routes/index.ts
@@ -1,6 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { funcionarioRoutes } from './funcionarioRoute'
+import { permissaoRoutes } from './permissaoRoute'
+import { dadosBancariosRoute } from '../routes/dadosBancariosRoute'
+import { contraChequeRoute } from '../routes/contraChequeRoute'
 
 export async function registerRHRoutes(app: FastifyInstance) {
   app.register(funcionarioRoutes, { prefix: 'funcionarios' })
+  app.register(permissaoRoutes, { prefix: 'permissoes' })
+  app.register(dadosBancariosRoute, { prefix: 'dados_bancarios' })
+  app.register(contraChequeRoute, { prefix: 'contra_cheque' })
 }
