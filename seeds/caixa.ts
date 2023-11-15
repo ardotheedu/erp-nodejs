a7c50e7aa716318f2a3de9a957fbddb4071b274b
Bernard2307 <bernardlnogueira@outlook.com>
2023-11-15 19:33:14 -0300

    fix: troquei o formato de data de caixa e mov_caixa

--- seeds/caixa.ts
+++ seeds/caixa.ts
@@ -1,4 +1,6 @@
 import { Knex } from "knex";
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat'
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
@@ -7,8 +9,8 @@ export async function seed(knex: Knex): Promise<void> {
   // Inserts seed entries
   await knex("caixa").insert([
     {
-      abertura: new Date(),
-      fechamento: null, // caixa aberto
+      abertura: dayjs('22/07/2023', 'DD/MM/YYYY').format(),
+      fechamento: dayjs('22/07/2023', 'DD/MM/YYYY').format(),
       saldo_inicial: 1000.0,
       suprimento: 0.0,
       sangria: 0.0,
@@ -16,8 +18,8 @@ export async function seed(knex: Knex): Promise<void> {
       saldo_fechamento: null,
     },
     {
-      abertura: new Date(),
-      fechamento: new Date(),
+      abertura: dayjs('20/07/2023', 'DD/MM/YYYY').format(),
+      fechamento: dayjs('20/07/2023', 'DD/MM/YYYY').format(),
       saldo_inicial: 1200.0,
       suprimento: 50.0,
       sangria: 30.0,
@@ -25,8 +27,8 @@ export async function seed(knex: Knex): Promise<void> {
       saldo_fechamento: 1190.0,
     },
     {
-      abertura: new Date(),
-      fechamento: new Date(),
+      abertura: dayjs('26/07/2023', 'DD/MM/YYYY').format(),
+      fechamento: dayjs('26/07/2023', 'DD/MM/YYYY').format(),
       saldo_inicial: 1500.0,
       suprimento: 20.0,
       sangria: 10.0,
@@ -34,8 +36,8 @@ export async function seed(knex: Knex): Promise<void> {
       saldo_fechamento: 1490.0,
     },
     {
-      abertura: new Date(),
-      fechamento: new Date(),
+      abertura: dayjs('29/08/2023', 'DD/MM/YYYY').format(),
+      fechamento: dayjs('29/08/2023', 'DD/MM/YYYY').format(),
       saldo_inicial: 800.0,
       suprimento: 60.0,
       sangria: 40.0,
@@ -43,8 +45,8 @@ export async function seed(knex: Knex): Promise<void> {
       saldo_fechamento: 780.0,
     },
     {
-      abertura: new Date(),
-      fechamento: new Date(),
+      abertura: dayjs('30/08/2023', 'DD/MM/YYYY').format(),
+      fechamento: dayjs('30/08/2023', 'DD/MM/YYYY').format(),
       saldo_inicial: 2000.0,
       suprimento: 70.0,
       sangria: 50.0,
@@ -52,8 +54,8 @@ export async function seed(knex: Knex): Promise<void> {
       saldo_fechamento: 1970.0,
     },
     {
-      abertura: new Date(),
-      fechamento: new Date(),
+      abertura: dayjs('01/09/2023', 'DD/MM/YYYY').format(),
+      fechamento: dayjs('01/09/2023', 'DD/MM/YYYY').format(),
       saldo_inicial: 600.0,
       suprimento: 30.0,
       sangria: 20.0,
@@ -61,8 +63,8 @@ export async function seed(knex: Knex): Promise<void> {
       saldo_fechamento: 590.0,
     },
     {
-      abertura: new Date(),
-      fechamento: new Date(),
+      abertura: dayjs('21/09/2023', 'DD/MM/YYYY').format(),
+      fechamento: dayjs('21/09/2023', 'DD/MM/YYYY').format(),
       saldo_inicial: 1800.0,
       suprimento: 40.0,
       sangria: 15.0,
@@ -70,8 +72,8 @@ export async function seed(knex: Knex): Promise<void> {
       saldo_fechamento: 1805.0,
     },
     {
-      abertura: new Date(),
-      fechamento: new Date(),
+      abertura: dayjs('22/09/2023', 'DD/MM/YYYY').format(),
+      fechamento: dayjs('22/09/2023', 'DD/MM/YYYY').format(),
       saldo_inicial: 950.0,
       suprimento: 25.0,
       sangria: 35.0,
@@ -79,17 +81,17 @@ export async function seed(knex: Knex): Promise<void> {
       saldo_fechamento: 915.0,
     },
     {
-      abertura: new Date(),
-      fechamento: null, // caixa aberto
+      abertura: dayjs('24/09/2023', 'DD/MM/YYYY').format(),
+      fechamento: dayjs('24/09/2023', 'DD/MM/YYYY').format(),
       saldo_inicial: 3000.0,
-      suprimento: 0.0,
-      sangria: 0.0,
+      suprimento: 10.0,
+      sangria: 15.0,
       saldo_atual: 3000.0,
-      saldo_fechamento: null,
+      saldo_fechamento: 945.0,
     },
     {
-      abertura: new Date(),
-      fechamento: new Date(),
+      abertura: dayjs('01/10/2023', 'DD/MM/YYYY').format(),
+      fechamento: dayjs('01/10/2023', 'DD/MM/YYYY').format(),
       saldo_inicial: 1750.0,
       suprimento: 10.0,
       sangria: 5.0,
--- seeds/movimentacao_caixa.ts
+++ seeds/movimentacao_caixa.ts
@@ -1,71 +1,74 @@
-import { randomUUID } from 'crypto'
-import { Knex } from 'knex'
+import { randomUUID } from 'crypto';
+import { Knex } from 'knex';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 
 export async function seed(knex: Knex): Promise<void> {
-  // Deletes ALL existing entries
-  await knex('movimentacao_caixa').del()
+  // Deleta TODAS as entradas existentes
+  await knex('movimentacao_caixa').del();
 
-  // Inserts seed entries
+  // Insere entradas manualmente
   await knex('movimentacao_caixa').insert([
     {
       id: randomUUID(),
-      timestamp: new Date(),
+      timestamp: dayjs('15/03/2023', 'DD/MM/YYYY').format(),
       tipo: 'entrada',
       valor: 100.00,
     },
     {
       id: randomUUID(),
-      timestamp: new Date(),
+      timestamp: dayjs('20/03/2023', 'DD/MM/YYYY').format(),
       tipo: 'saida',
       valor: 50.00,
     },
     {
       id: randomUUID(),
-      timestamp: new Date(),
+      timestamp: dayjs('22/03/2023', 'DD/MM/YYYY').format(),
       tipo: 'devolucao',
       valor: 25.00,
     },
     {
       id: randomUUID(),
-      timestamp: new Date(),
+      timestamp: dayjs('25/03/2023', 'DD/MM/YYYY').format(),
       tipo: 'entrada',
       valor: 75.00,
     },
     {
       id: randomUUID(),
-      timestamp: new Date(),
+      timestamp: dayjs('28/03/2023', 'DD/MM/YYYY').format(),
       tipo: 'saida',
       valor: 30.00,
     },
     {
       id: randomUUID(),
-      timestamp: new Date(),
+      timestamp: dayjs('02/04/2023', 'DD/MM/YYYY').format(),
       tipo: 'devolucao',
       valor: 15.00,
     },
     {
       id: randomUUID(),
-      timestamp: new Date(),
+      timestamp: dayjs('05/04/2023', 'DD/MM/YYYY').format(),
       tipo: 'entrada',
       valor: 120.00,
     },
     {
       id: randomUUID(),
-      timestamp: new Date(),
+      timestamp: dayjs('10/04/2023', 'DD/MM/YYYY').format(),
       tipo: 'saida',
       valor: 60.00,
     },
     {
       id: randomUUID(),
-      timestamp: new Date(),
+      timestamp: dayjs('15/04/2023', 'DD/MM/YYYY').format(),
       tipo: 'entrada',
       valor: 90.00,
     },
     {
       id: randomUUID(),
-      timestamp: new Date(),
+      timestamp: dayjs('20/04/2023', 'DD/MM/YYYY').format(),
       tipo: 'saida',
       valor: 45.00,
     },
-  ])
+  ]);
 }
+
