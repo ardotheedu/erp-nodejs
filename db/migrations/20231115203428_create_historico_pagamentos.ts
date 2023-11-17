f8d229ceab291b4e89d6a920b2a582dd856ac605
Bernard2307 <bernardlnogueira@outlook.com>
2023-11-17 14:55:43 -0300

    .

--- db/migrations/20231115203428_create_historico_pagamentos.ts
+++ db/migrations/20231115203428_create_historico_pagamentos.ts
@@ -10,6 +10,7 @@ export async function up(knex: Knex): Promise<void> {
     table.decimal('valor', 10, 2).notNullable();
   });
 
+  //só para testar
   await knex('historico_pagamentos').insert([
     { id: '1', cliente_id: 1, data: new Date(), descricao: 'Pagamento 1', valor: 100.00 },
     { id: '2', cliente_id: 2, data: new Date(), descricao: 'Pagamento 2', valor: 150.00 },
