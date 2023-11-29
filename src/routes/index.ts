909cb39ba8879aa1c086598817b9027ea7fd2f7f
Pedrojuaba <phgcoelho@outlook.com>
2023-11-29 20:48:08 -0300

    feat: implementação das rotas em app e em index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
-export { registerStockRoutes } from '../Modules/Stock/Routes'
-export { registerRHRoutes } from '../Modules/RH/Routes'
+export { registerStockRoutes } from "../Modules/Stock/Routes";
+export { registerRHRoutes } from "../Modules/RH/Routes";
 // export { rhRoutes } from './rh'
-export { clienteRoutes } from './cliente'
-export { itemPedidoRoutes } from './item_pedido'
-export { pedidoRoutes } from './pedido'
-export { vendaSaidaRoutes } from './venda_saida'
-export { lancamentoRoutes } from './lancamento'
-export { notaFiscalRoutes } from './notafiscal'
-export { caixaRoutes } from './caixa'
-export { permissaoRoutes } from './permissao'
-export { movimentacaoCaixaRoutes } from './movimentacao_caixa'
-export { dashboardRoutes } from './dashboard'
+export { clienteRoutes } from "./cliente";
+export { itemPedidoRoutes } from "./item_pedido";
+export { pedidoRoutes } from "./pedido";
+export { vendaSaidaRoutes } from "./venda_saida";
+export { lancamentoRoutes } from "./lancamento";
+export { notaFiscalRoutes } from "./notafiscal";
+export { caixaRoutes } from "./caixa";
+export { permissaoRoutes } from "./permissao";
+export { movimentacaoCaixaRoutes } from "./movimentacao_caixa";
+export { dashboardRoutes } from "./dashboard";
+export { alimentosEntradaRoutes } from "./alimentos_entrada";
+export { alimentosSaidaRoutes } from "./alimentos_saida";
