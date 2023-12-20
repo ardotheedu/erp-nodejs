83a20575140eabcfeef7dd61a990e1b738538216
Bernard2307 <bernardlnogueira@outlook.com>
2023-12-19 22:35:14 -0300

    f

--- db/migrations/20231128203014_alimentos.ts
+++ db/migrations/20231128203014_alimentos.ts
@@ -4,11 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("alimentos", (table) => {
     table.uuid("id").primary();
     table.string("nome").notNullable();
-    table
-      .integer("unidade_medida_id")
-      .unsigned()
-      .references("id")
-      .inTable("unidade_medida");
+    table.string("unidade_medida").notNullable();
     table.integer("quantidade_em_estoque").notNullable();
   });
 }
